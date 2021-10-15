59264db30a02e8b57c56c790ae9a82960e149198
Brett Moyo <moyobrett@gmail.com>
2021-10-15 21:14:39 +0200

    changed conditional statements

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+let detailedBuy = {};
+let purchasedItems = [];
+
 const infoOnItems = () => {
     // initializing a variable that gives out random values
     let price = (Math.random() * 100.99).toFixed(2);
@@ -6,28 +9,47 @@ const infoOnItems = () => {
     // getting the selected value
     let value = noOptionYet.value;
 
-    // location.reload();
-   
-// if the selected value is 0 fthe the price and item should be 0 else the items and numbers should be randomized declare above.
-        if(value == "0"){
+    // if the selected value is 0 fthe the price and item should be 0 else the items and numbers should be randomized declare above.
+    if (value == "0") {
         document.getElementById("items").value = 0;
         document.getElementById("price").value = `R00.00`;
-       }else{
+
+    } else if (value == "1") {
         document.getElementById("items").value = `${items}`;
         document.getElementById("price").value = `${price}`;
-       }
+        detailedBuy = {
+            "product": "Product01",
+            "items": items,
+            "price": price
+        };
+        localStorage.setItem("detailed", JSON.stringify(detailedBuy))
+
+    } else if (value == "2") {
+        document.getElementById("items").value = `${items}`;
+        document.getElementById("price").value = `${price}`;
+        detailedBuy = {
+            "product": "Product02",
+            "items": items,
+            "price": price
+        };
+        localStorage.setItem("detailed", JSON.stringify(detailedBuy))
+    } else {
+        document.getElementById("items").value = `${items}`;
+        document.getElementById("price").value = `${price}`;
+        detailedBuy = {
+            "product": "Product03",
+            "items": items,
+            "price": price
+        };
+        localStorage.setItem("detailed", JSON.stringify(detailedBuy))
+    }
 
 
-    // save the price and items on the local storage
-    localStorage.setItem("items",items)
-    localStorage.setItem("price", price)
+
 
 };
 const afterSubmit = () => {
-    // retrieve the items  frome the storage  to alert the user of how much has been purchased.
-    let itemDetail = localStorage.getItem("items");
-    let priceDetails = localStorage.getItem("price");
+    // retrieve the items  frome the storage  to alert the user of how much has been purchased
 
     alert(`you just purchased ${itemDetail} items for R${priceDetails} per item. Total price: R${(itemDetail * priceDetails).toFixed(2)}`);
 }
-
